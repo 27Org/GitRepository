2e9c04a0f154d1d5fab38fa17297cb398dabc60b
zhulin0 <32284721+zhulin0@users.noreply.github.com>
2017-10-21 22:39:13 +0800

    PortName.java

---
+++ PortName.java
@@ -0,0 +1,19 @@
+package com.work.service;
+
+public class PortName {
+	private int id;
+	private String portName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPortName() {
+		return portName;
+	}
+	public void setPortName(String portName) {
+		this.portName = portName;
+	}
+	
+}
